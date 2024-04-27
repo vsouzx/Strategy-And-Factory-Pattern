c64bbbd66a45c9eadc58b558e7ebcd45eb4534cf
vsouzx_ <vtsoliveira2001@gmail.com>
2024-04-26 22:56:01 -0300

    ajustando passtas

---
+++ strategy-example-1/src/main/java/br/com/souza/strategyfactory/example/strategy/impl/dassadadasDiscount.java
@@ -0,0 +1,35 @@
+package br.com.souza.strategyfactory.example.strategy.impl;
+
+import br.com.souza.strategyfactory.example.dto.ProductDiscountResponse;
+import br.com.souza.strategyfactory.example.model.ProductEntity;
+import br.com.souza.strategyfactory.example.strategy.DiscountStrategy;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class dassadadasDiscount implements DiscountStrategy {
+
+    @Override
+    public ProductDiscountResponse calcularDesconto(ProductEntity product) {
+        BigDecimal discount = BigDecimal.ZERO;
+
+        // Outras regras e validações...
+        if(product.getMinClientLevelForDiscount() <= 1 && product.getPrice().compareTo(new BigDecimal("2000")) > 0){
+            discount = product.getPrice().multiply(new BigDecimal("0.1"));
+        }
+
+        return ProductDiscountResponse.builder()
+                .productName(product.getName())
+                .priceWithoutDiscount(product.getPrice())
+                .priceWithDiscount(product.getPrice().subtract(discount))
+                .discountQuantity(discount)
+                .build();
+    }
+
+    @Override
+    public Integer getLevelDiscount() {
+        return 5;
+    }
+
+}
